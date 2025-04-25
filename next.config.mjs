e3e9babfee01714c3e0cc93321ad66292419c304
Mayron Fernandes <mayron.fernandes@alghorit.com.br>
2025-04-25 16:05:32 -0300

    feat: remove unused env

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-import "./src/env/index.js"
-
 const nextConfig = {
   // if you want to use standalone output, uncomment the following line
   // transpilePackages: ["@t3-oss/env-nextjs", "@t3-oss/env-core"],
