204a7755e1db029c4e292bec40f60af10a0599cd
mayron1806 <mayronfernandes01@gmail.com>
2025-04-30 07:30:53 -0300

    chore: create my next authenticator

--- packages/prisma/client.ts
+++ packages/prisma/client.ts
@@ -1,1 +1,9 @@
-export * from "@prisma/client";
+export type { 
+  User,
+  Role,
+  Workspace,
+  Member,
+  Invite,
+  Account,
+  VerificationToken,
+} from "@prisma/client"
--- packages/prisma/index.ts
+++ packages/prisma/index.ts
@@ -10,3 +10,4 @@ const client = globalThis.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
 
 export { client as prisma };
+export * from "./client";
