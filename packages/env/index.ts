4e48bfe0af8daef24b9e7a83da953995a50c0ee9
Mayron Fernandes <mayron.fernandes@alghorit.com.br>
2025-05-05 10:53:02 -0300

    fix: __dirname not found

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -1,6 +1,10 @@
 import dotenv from "dotenv";
 import path from "path";
+import { fileURLToPath } from "url";
 import { z } from "zod";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
