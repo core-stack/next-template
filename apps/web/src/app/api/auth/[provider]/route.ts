a897caa857e115a46548bcb05e9f282b3c018e1f
Mayron Fernandes <mayron.fernandes@alghorit.com.br>
2025-05-05 12:22:27 -0300

    fix: oauth2 provider error

--- apps/web/src/app/api/auth/[provider]/route.ts
+++ apps/web/src/app/api/auth/[provider]/route.ts
@@ -3,6 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(_: NextRequest, { params }: { params: Promise<{ provider: string }> }) {
   const { provider } = await params
-  const url = await auth.providers[provider].getAuthUrl();
+  const url = await auth.oauth2GetUrl(provider);
   return NextResponse.redirect(url);
 }
--- apps/web/src/app/api/auth/callback/[provider]/route.ts
+++ apps/web/src/app/api/auth/callback/[provider]/route.ts
@@ -1,5 +1,4 @@
 import { auth } from "@/lib/auth";
-import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest, { params }: { params: Promise<{ provider: string }> }) {
@@ -7,14 +6,5 @@ export async function GET(req: NextRequest, { params }: { params: Promise<{ prov
   const { provider } = await params;
   if (!code) return NextResponse.json({ error: 'Missing code' }, { status: 400 });
 
-  const res = await auth.providers[provider].callback(code);
-  const token = auth.jwt.generateTokens(res.user);
-
-  // set cookies
-  const { accessToken, refreshToken, accessTokenDuration, refreshTokenDuration } = token;
-  const cookie = await cookies();
-  cookie.set("access-token", accessToken, { maxAge: accessTokenDuration });
-  cookie.set("refresh-token", refreshToken, { maxAge: refreshTokenDuration });
-
-  return NextResponse.redirect(new URL('/', req.url));
+  return await auth.oauth2Callback(req, provider);
 }
